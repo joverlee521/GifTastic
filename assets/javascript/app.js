2e0db6fa1ffa29da731074ce240d161ad57870aa
J.Lee <joverlee521@gmail.com>
2018-09-11 15:22:28 -0700

    call on API to generate gifs when button is clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var topics = ["Big Bang", "Super Junior", "Girl's Generation", "TVXQ", "BTS", "MAMAMOO", "EXO", "B.A.P.", "BLACKPINK", "SHINee", "EXID", "AOA", "f(x)"]
+var topics = ["Big Bang", "Super Junior", "Girl's Generation", "TVXQ", "BTS", "MAMAMOO", "EXO", "B.A.P.", "BLACKPINK", "SHINee", "EXID", "AOA", "f(x)", "MBLAQ", "Block B", "Epik High"]
 
 function createButtons(){
     for(var i = 0; i < topics.length; i++){
@@ -9,6 +9,25 @@ function createButtons(){
     }
 }
 
+function clickButton(){
+    $(".btn").on("click", function(){
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).text() + "+kpop&api_key=10dpuSkxshqdLim7xp7FyDbgQPlJC3Vc&limit=10";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(result){
+            $("#gifs").empty();
+            for(var i = 0; i < result.data.length; i++){
+                var newImage = $("<img>");
+                newImage.addClass("m-1 gifImage");
+                newImage.attr("src", result.data[i].images.fixed_height_still.url);
+                $("#gifs").append(newImage);
+            }
+        })
+    })
+}
+
 $(document).ready(function(){
     createButtons();
+    clickButton();
 })
