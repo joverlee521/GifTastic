880b13654212e2a6d3c7e31b18d3191f86fb0037
J.Lee <joverlee521@gmail.com>
2018-09-11 18:57:52 -0700

    displays gifs with ratings under them

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-var topics = ["Big Bang", "Super Junior", "Girl's Generation", "TVXQ", "BTS", "MAMAMOO", "EXO", "B.A.P.", "BLACKPINK", "SHINee", "EXID", "AOA", "f(x)", "MBLAQ", "Block B", "Epik High"];
+var topics = ["Big Bang", "Super Junior", "Girl's Generation", "TVXQ", "BTS", "MAMAMOO", "EXO", "BAP", "BLACKPINK", "SHINee", "EXID", "AOA", "f(x)", "MBLAQ", "BlockB", "Epik High"];
 
 var gifTastic = {
     createButtons(){
@@ -10,6 +10,7 @@ var gifTastic = {
         }
     },
     clickButton(){
+        var that = this;
         $(".btn").on("click", function(){
             var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + $(this).text() + "+kpop&api_key=10dpuSkxshqdLim7xp7FyDbgQPlJC3Vc&limit=10";
             $.ajax({
@@ -18,31 +19,52 @@ var gifTastic = {
             }).then(function(result){
                 $("#gifs").empty();
                 for(var i = 0; i < result.data.length; i++){
-                    var gif = result.data[i].images.fixed_height_still.url;
+                    var gif = result.data[i].images.fixed_width_still.url;
+                    var rating = result.data[i].rating.toUpperCase();
+                    var imageRating = $("<span>");
+                    imageRating.text("Rating: " + rating);
                     var newImage = $("<img>");
                     newImage.addClass("m-1 gifImage");
                     newImage.attr({"value": i, "src": gif, "status": "static"});
-                    $("#gifs").append(newImage);
+                    var newDiv = $("<div>");
+                    newDiv.addClass("col-3 text-center")
+                    newDiv.append(newImage);
+                    newDiv.append("<br>");
+                    newDiv.append(imageRating);
+                    $("#gifs").append(newDiv);
                 }
-                gifTastic.clickImage();
+                that.clickImage();
             })
         })
     },
     clickImage(){
         $(".gifImage").on("click", function(){
             if($(this).attr("status") === "static"){
-                this.src = this.src.replace("200_s", "200");
+                this.src = this.src.replace("200w_s", "200w");
                 $(this).attr("status", "gif");
             }
             else{
-                this.src = this.src.replace("200", "200_s");
+                this.src = this.src.replace("200w", "200w_s");
                 $(this).attr("status", "static");
             }
         })
+    },
+    collectInput(){
+        var that = this;
+        $("#submit").on("click", function(){
+            var input = $("#search").val();
+            $("#search").val("");
+            var newButton = $("<button>");
+            newButton.addClass("btn btn-outline-info m-1");
+            newButton.text(input);
+            $("#buttons").append(newButton);
+            that.clickButton();
+        });
     }
 }
 
 $(document).ready(function(){
     gifTastic.createButtons();
     gifTastic.clickButton();
+    gifTastic.collectInput();
 })
